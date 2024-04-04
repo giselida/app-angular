c969ec02a6c0660943f56cd0f440e2c14e11d204
Gisélida Melo <giselidac@gmail.com>
2024-04-03 21:29:39 -0300

    ✨ feat: created NotFoundComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './domain/auth/page/auth/auth.component';
 import { TransactionComponent } from './domain/transaction/transaction.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -16,5 +17,10 @@ export const routes: Routes = [
   {
     path: 'transaction',
     loadComponent: () => TransactionComponent,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    loadComponent: () => NotFoundComponent,
   },
 ];
---
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [CommonModule, RouterModule, MatButtonModule],
+  template: `<div class="not-found-container">
+      <span class="glitch">404</span>
+      <span class="offset">404</span>
+      <span>404</span>
+    </div>
+    <p>
+      A página que você procura pode ter sido removida, seu URL alterado ou
+      estar temporariamente indisponível.
+    </p>
+    <button mat-raised-button color="primary" [routerLink]="['/transaction']">
+      Pagina inicial
+    </button> `,
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {}
